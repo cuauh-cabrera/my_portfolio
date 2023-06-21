fc5ca6899b4e571cfbd168bf382db292be095e02
CuauCabrera <cabrera.cuau@gmail.com>
2023-06-21 09:20:47 -0600

    Add mobile modal window for projects section

--- main.js
+++ main.js
@@ -13,3 +13,50 @@ closeMenu.addEventListener('click', () => mainMenu.classList.toggle('menu-show')
 mainMenu.addEventListener('click', () => mainMenu.classList.toggle('menu-show'));
 mainMenu.addEventListener('click', () => { iconMenu.style.display = 'flex'; });
 closeMenu.addEventListener('click', () => mainMenu.classList.toggle('menu-show'));
+
+/* Project's modal window */
+
+/// Variables
+const openModalButton = document.querySelectorAll('[data-modal-target]');
+const closeModalButton = document.querySelectorAll('[data-close-modal]');
+const overlay = document.getElementById('overlay');
+
+/// Events and methods
+
+/// Open modal
+openModalButton.forEach(button => {
+    button.addEventListener('click',() => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal)
+    })
+})
+
+/// Active overlay
+overlay.addEventListener('click',() => {
+    const modals = document.querySelectorAll('.projects__modal--conatiner.active')
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+/// Close modal
+closeModalButton.forEach(button => {
+    button.addEventListener('click',() => {
+        const modal = button.closest('.projects__modal--conatiner');
+        closeModal(modal);
+
+    })
+})
+
+/// Functions
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if(modal == null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
